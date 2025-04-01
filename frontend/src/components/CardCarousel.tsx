6c80fc36213bf2524fd95443ea25dfcd2b73374d
Nyx <lilizhao107513@gmail.com>
2025-04-01 14:33:20 -0400

    updates on card carousel

--- frontend/src/components/CardCarousel.tsx
+++ frontend/src/components/CardCarousel.tsx
@@ -1,15 +1,21 @@
 function CardCarousel(){
     return(
-        <div className="carousel carousel-center shadow">
-            <div className="carousel-item w-1/2 p-6">
+        <div>
+            <header className="flex items-center justify-center font-display font-bold py-10">
+                <h1 className="text-3xl">Projects</h1>
+            </header>
+        
+
+        <div className="carousel carousel-center bg-wine shadow">
+            <div className="carousel-item w-1/2 p-4">
                 
-                <div className="flip-card group w-full h-auto relative">
+                <div className="flip-card group w-full h-full relative">
                     <div className="flip-inner w-full h-full transition-transform duration-500 group-hover:rotate-y-180 transform-style-preserve-3d overflow:hidden object-contain">
                 
                         {/* Front Side */}
                         <div className="absolute w-full h-full backface-hidden bg-white">
                             <img
-                                className="w-full h-full object-contain"
+                                className="w-full h-full p-5 object-contain object-fit:cover"
                                 src="livablestreetslogo.png"
                                 alt="Livable Streets Alliance logo"
                             />
@@ -19,11 +25,25 @@ function CardCarousel(){
                         {/* Back Side */}
                         <div className="absolute w-full h-full backface-hidden rotate-y-180 bg-white p-6 flex flex-col justify-center items-center text-center">
                             <p className="mb-2 text-sm text-gray-700">
-                                Data analysis of displacement trends and correlations such as __,__,__ using machine learning pipeline workflow for the client LivableStreets Alliance. 
+                                <span className="font-bold">LivableStreets:Bike Infrastructure </span>
+                                is an extensive 
+                                <span className="font-bold"> data analysis pipeline </span> 
+                                to investigate key drivers of
+                                <span className="font-bold"> displacement trends </span>
+                                in the Greater Boston area for the nonprofit organization 
+                                <a href="https://www.livablestreets.info" className=" hover:underline font-medium"> LivableStreets Alliance </a>
+                            </p>
+                            <p className="mb-2 text-sm text-gray-700">
+                                It uses
+                                <span className="font-bold"> machine learning </span>and 
+                                <span className="font-bold"> geospatial techniques </span>in 
+                                <span className="font-bold"> Python </span>
+                                (including libraries such as
+                                <span className="font-bold">  numpy, pandas, geopandas, sckit-learn, matplotlib,</span> etc.)
                             </p>
                             <a
-                                href="#"
-                                className="text-blue-500 hover:underline font-medium"
+                                href="https://github.com/BU-Spark/ds-livable-streets-infra/tree/team-c-final"
+                                className="text-terracota hover:underline font-medium"
                             >
                                 Explore More →
                             </a>
@@ -33,8 +53,8 @@ function CardCarousel(){
             </div>
         
         
-            <div className="carousel-item w-1/2">
-                <div className="flip-card group w-full h-auto relative">
+            <div className="carousel-item w-1/2 p-4">
+                <div className="flip-card group w-full  h-full relative">
                     <div className="flip-inner w-full h-full transition-transform duration-500 group-hover:rotate-y-180 transform-style-preserve-3d overflow:hidden object-contain">
                 
                         {/* Front Side */}
@@ -50,11 +70,22 @@ function CardCarousel(){
                         {/* Back Side */}
                         <div className="absolute w-full h-full backface-hidden rotate-y-180 bg-white p-6 flex flex-col justify-center items-center text-center">
                             <p className="mb-2 text-sm text-gray-700">
-                                Mobile application Unwind with features including __,__,__. Designed in Figma, (maybe include link to Figma design)
+                                <span className="font-bold">Unwind </span>
+                                is a mobile wellness app designed with a focus on mindfulness and relaxation in 
+                                <span className="font-bold"> Kotlin</span>. 
                             </p>
+                            <p className="mb-2 text-sm text-gray-700">
+                                It provides a 
+                                <span className="font-bold"> user-friendly interface</span> to help users track their mood, journal their thoughts, and explore relaxation techniques through a 
+                                <span className="font-bold"> clean and intuitive interface</span> designed in 
+                                <span className="font-bold"> Figma</span>.
+                            </p>
+                            
+                            
+
                             <a
-                                href="#"
-                                className="text-blue-500 hover:underline font-medium"
+                                href="https://github.com/karlav24/Unwind"
+                                className="text-terracota hover:underline font-medium"
                             >
                                 Explore More →
                             </a>
@@ -88,6 +119,7 @@ function CardCarousel(){
             src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
             alt="Burger" />
         </div>
+      </div>
       </div>
     );
 }
