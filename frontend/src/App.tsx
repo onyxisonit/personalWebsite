8142476e194a7b237f7108b665a6ce8fcf1cbe8a
Nyx <lilizhao107513@gmail.com>
2025-04-11 18:53:50 -0400

    set up framer motion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useState } from 'react'
 
 import './App.css'
 import './index.css'
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
+import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
 
   return (
     <>
+    
       <Router>
-        <div className="scroll-smooth">
+        <div className="App scroll-smooth">
           <Nav />
-        
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-
+          <AnimatedRoutes />
           <Footer />
         </div> 
       </Router>
